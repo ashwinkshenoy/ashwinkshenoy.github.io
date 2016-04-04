c9b5c01dcdceaf7820e99978adb5bae8640009ec
ashwinshenoy <ashwinkshenoy@gmail.com>
2016-04-04 10:43:18 +0530

    new mail links

--- js/animation.js
+++ js/animation.js
@@ -34,7 +34,7 @@ $(document).ready(function($) {
   var scene2 = new ScrollScene({
     triggerElement: ".jumbo1",
     triggerHook:'onLeave',
-    reverse:true, 
+    reverse:true,
     offset:300,
     duration:1500
   })
@@ -42,7 +42,7 @@ $(document).ready(function($) {
   .setPin(".section2")
   .addTo(controller);
   //scene2.addIndicators({zindex: 100, colorEnd: "red"});
-  //scene 2 end  
+  //scene 2 end
 
   //scene 3
   var pinani3 = new TimelineMax()
@@ -57,7 +57,7 @@ $(document).ready(function($) {
   var scene3 = new ScrollScene({
     triggerElement: ".jumbo2",
     triggerHook:'onLeave',
-    reverse:true, 
+    reverse:true,
     offset:1650,
     duration:1500
   })
@@ -65,7 +65,7 @@ $(document).ready(function($) {
   .setPin(".section3")
   .addTo(controller);
   //scene3.addIndicators({zindex: 100, colorEnd: "red"});
-  //scene 3 end 
+  //scene 3 end
 
   //scene 4
   var pinani4 = new TimelineMax()
@@ -79,7 +79,7 @@ $(document).ready(function($) {
   var scene4 = new ScrollScene({
     triggerElement: ".jumbo3",
     triggerHook:'onLeave',
-    reverse:true, 
+    reverse:true,
     offset:1800,
     duration:1000
   })
@@ -87,7 +87,7 @@ $(document).ready(function($) {
   .setPin(".section4")
   .addTo(controller);
   //scene4.addIndicators({zindex: 100, colorEnd: "red"});
-  //scene 4 end  
+  //scene 4 end
 
   //scene 5
   var pinani5 = new TimelineMax()
@@ -95,14 +95,15 @@ $(document).ready(function($) {
       TweenMax.to("#body", 1,{delay:0.05,opacity:1,'background-color':'#16B1EC'}),
       TweenMax.from("#sec5-hd", 0.5,{opacity:0,top:'200px',position:'relative'},{opacity:1}),
       TweenMax.from("#social1", 0.5,{delay:0.3,opacity:0,top:'200px',left:'-200px',position:'relative'},{opacity:1}),
-      TweenMax.from("#social2", 0.5,{delay:0.3,opacity:0,top:'200px',position:'relative'},{opacity:1}),
-      TweenMax.from("#social3", 0.5,{delay:0.3,opacity:0,top:'200px',left:'200px',position:'relative'},{opacity:1}),
+      TweenMax.from("#social2", 0.5,{delay:0.3,opacity:0,top:'200px',left:'-50px',position:'relative'},{opacity:1}),
+      TweenMax.from("#social3", 0.5,{delay:0.3,opacity:0,top:'200px',left:'50px',position:'relative'},{opacity:1}),
+      TweenMax.from("#social4", 0.5,{delay:0.3,opacity:0,top:'200px',left:'200px',position:'relative'},{opacity:1}),
     ]);
   //build scene
   var scene5 = new ScrollScene({
     triggerElement: ".jumbo4",
     triggerHook:'onLeave',
-    reverse:true, 
+    reverse:true,
     offset:1100,
     duration:1000
   })
@@ -110,7 +111,7 @@ $(document).ready(function($) {
   .setPin(".section5")
   .addTo(controller);
   //scene4.addIndicators({zindex: 100, colorEnd: "red"});
-  //scene 4 end  
+  //scene 4 end
 
 
 });
