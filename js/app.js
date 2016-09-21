fdaf379bc1d6a66d1c266d7e908b83dda2399421
ashwinshenoy <ashwinkshenoy@gmail.com>
2016-09-21 15:03:33 +0530

    New mobile changes

--- js/app.js
+++ js/app.js
@@ -1,3 +1,24 @@
-$(document).ready(function($) {
+// $(document).ready(function($) {
 
-});
+// });
+
+WebFontConfig = {
+  google: { families: [ 'Open+Sans:400,300,600,700,800' ] }
+};
+
+var cb = function() {
+  var wf = document.createElement('script');
+  wf.src = '//ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+  wf.type = 'text/javascript';
+  wf.async = 'true';
+  var s = document.getElementsByTagName('script')[0];
+  s.parentNode.insertBefore(wf, s);
+};
+
+var raf = requestAnimationFrame || mozRequestAnimationFrame || webkitRequestAnimationFrame || msRequestAnimationFrame;
+
+if(raf){
+  raf(cb);
+}else{
+  window.addEventListener('load', cb);
+}
